ac16b588dc9fc1162f7b4f8c3766b1db0ccbd2f3
Matheus Silveira <blanchimaah@gmail.com>
2020-03-29 17:25:43 -0300

    Testes, jest, etc

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,16 +1,15 @@
-const express = require("express");
-const routes = require('./routes');
-const cors = require('cors');
-const app = express();
+const express  = require("express");
+const routes   = require('./routes');
+const cors     = require('cors');
+const {errors} = require('celebrate');
+const app      = express();
 
-
-/*
-Avisando ao express que o corpo da requisição está em formato JSON
-*/ 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
+module.exports = app;
 
 /*
 * Métodos HTTP
@@ -34,4 +33,3 @@ app.use(routes);
  * Driver: SELECT * FROM users
  * Query Builder: table('users').select('*').where()
  */
-app.listen(3333);
